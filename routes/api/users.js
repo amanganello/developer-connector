74997ab6adb968d35942a664b7b025f296f712c4
Alvaro Manganello <amanganello@altimetrik.com>
2020-02-20 09:05:37 -0300

    Adding create delete for experience, education, getting github repos

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,15 +8,6 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-//  @route      GET api/users/test
-//  @desc       Tests users route
-//  @access     Public
-router.get('/test', (req, res) =>
-    res.json({
-        msg: 'Users works'
-    })
-);
-
 //  @route      POST     api/users/register
 //  @desc       Register user
 //  @access     Public
